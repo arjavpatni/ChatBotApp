3a13322ab3a8cf7012dece052598d3ca98128f4c
Arjav <arjavpatni@hotmail.com>
2020-02-20 00:25:33 +0530

    MessageService updated.

--- app/src/main/java/com/arjavp/chatbotapp/MainActivity.kt
+++ app/src/main/java/com/arjavp/chatbotapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.arjavp.chatbotapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.arjavp.chatbotapp.Utilities.BASE_URL
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var messageAdapter : MessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/arjavp/chatbotapp/Services/MessageService.kt
+++ app/src/main/java/com/arjavp/chatbotapp/Services/MessageService.kt
@@ -6,7 +6,9 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
 import com.arjavp.chatbotapp.Model.MessageUi
+import com.arjavp.chatbotapp.Utilities.API_KEY
 import com.arjavp.chatbotapp.Utilities.BASE_URL
+import com.arjavp.chatbotapp.Utilities.chatBotID
 import org.json.JSONException
 
 object MessageService {
@@ -14,13 +16,12 @@ object MessageService {
     val messages = mutableListOf<MessageUi>()
 
     fun getMessages(context: Context, complete: (Boolean)-> Unit){
-        val url = BASE_URL
+        val url = "${BASE_URL}?apiKey=${API_KEY}&chatbotID=${chatBotID}&externalID=Arjav&message="
         val messagesRequest = object: JsonArrayRequest(Method.GET, url, null, Response.Listener { response ->
             try{
                 for(x in 0 until response.length()){
                     val message = response.getJSONObject(x)
                     val messageBody = message.getString("message")
-                    val chatBotId = message.getString("chatBotID")
 
                     val newMessage = MessageUi(messageBody, MessageUi.RECEIVED_MSG)
                     messages.add(newMessage)
